85434303bef9b2a84eb82cb4e3a4924c4d793389
Berkat Saragih <berkat.sragih@gmail.com>
2024-08-13 23:13:54 +0700

    feat: add project router into project domain

--- internal/project/router.go
+++ internal/project/router.go
@@ -1,1 +1,11 @@
 package project
+
+import "net/http"
+
+func RegisterRoutes(router *http.ServeMux, handler *ProjectController) {
+	router.HandleFunc("GET /projects", handler.FindAll)
+	//router.HandleFunc("GET /projects/{id}", handler.FindProjectByID)
+	router.HandleFunc("POST /projects", handler.CreateProject)
+	router.HandleFunc("PUT /projects/{id}", handler.UpdateProject)
+	router.HandleFunc("DELETE /projects/{id}", handler.DeleteProject)
+}
