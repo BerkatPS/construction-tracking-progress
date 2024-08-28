4844fda160a710959605e3af78bd7147fd9b1940
BerkatPS <berkat.sragih@gmail.com>
2024-08-28 07:52:55 +0000

    feat: add new method for middleware

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -59,3 +59,26 @@ func CORSHandler(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func IPMiddleware(allowedIPs []string) func(http.Handler) http.Handler {
+	return func (next http.Handler) http.Handler  {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ip := r.Header.Get("X-Forwarded-For")
+			if ip == "" {
+				ip = r.RemoteAddr
+			}
+			r.Header.Set("X-Forwarded-For", ip)
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+
+func isAllowedIP(ip string, allowedIPs []string) bool {
+	for _, allowedIP := range allowedIPs {
+		if ip == allowedIP {
+			return true
+		}
+	}
+	return false
+}
